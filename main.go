e369cec3917127d13bd8a5b89e1c8577d5f5f151
utkarshmani1997 <utkarshmani1997@gmail.com>
2017-10-18 11:19:03 +0530

    First Commit

--- main.go
+++ main.go
@@ -2,104 +2,56 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
-
-	"github.com/utkarshmani1997/openebs_exporter/collector"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/utkarshmani1997/openebs_exporter/collector"
 )
-
-// Version of nsq_exporter. Set at build time.
-const Version = "0.0.0.dev"
 
 var (
 	listenAddress        = flag.String("web.listen", ":9500", "Address on which to expose metrics and web interface.")
 	metricsPath          = flag.String("web.path", "/metrics", "Path under which to expose metrics.")
-	OpenEBSControllerURL = flag.String("controller.addr", "http://localhost:9501/v1/stats", "Address of the controller.")
-	namespace            = flag.String("namespace", "OpenEBS", "Namespace for the OpenEBS metrics.")
-
-	enabledCollectors = flag.String("collect", "stats.OpenEBS", "Comma-separated list of collectors to use.")
-	statsRegistry     = map[string]func(namespace string) collector.StatsCollector{
-		"OpenEBS": collector.VolumeStats,
-	}
+	openEBSControllerURL = flag.String("controller.addr", "http://localhost:9501/", "Address of the OpenEBS controller monitoring.")
+	namespace            = flag.String("namespace", "OpenEBS Controller", "Namespace for the OpenEBS Volume metrics.")
+	homepage             = `<html>
+			                <head><title>OpenEBS Exporter</title></head>
+			                <body>
+			                <h1>OpenEBS Exporter</h1>
+			                <p><a href="` + *metricsPath + `">Metrics</a></p>
+			                </body>
+			                </html>`
 )
 
 func main() {
 	flag.Parse()
+	controllerURL, err := url.Parse(*openEBSControllerURL)
 
-	ex, err := createOpenEBSExecutor()
 	if err != nil {
-		log.Fatalf("error creating nsq executor: %v", err)
+		log.Fatal(err)
 	}
-	prometheus.MustRegister(ex)
 
-	http.Handle(*metricsPath, promhttp.Handler())
-	if *metricsPath != "" && *metricsPath != "/" {
+	exporter := collector.NewExporter(controllerURL)
+	prometheus.MustRegister(exporter)
+
+	log.Printf("Starting Server: %s", *listenAddress)
+	if *metricsPath == "" || *metricsPath == "/" {
+
+		http.Handle(*metricsPath, promhttp.Handler())
+
+	} else {
+
+		http.Handle(*metricsPath, promhttp.Handler())
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte(`<html>
-			<head><title>OpenEBS Exporter</title></head>
-			<body>
-			<h1>OpenEBS Exporter</h1>
-			<p><a href="` + *metricsPath + `">Metrics</a></p>
-			</body>
-			</html>`))
+
+			w.Write([]byte(homepage))
 		})
 	}
 
-	log.Print("listening to ", *listenAddress)
 	err = http.ListenAndServe(*listenAddress, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func createOpenEBSExecutor() (*collector.Executor, error) {
-
-	openEBSControllerURL, err := normalizeURL(*OpenEBSControllerURL)
-	if err != nil {
-		return nil, err
-	}
-
-	ex := collector.NewExecutor(*namespace, openEBSControllerURL)
-
-	for _, param := range strings.Split(*enabledCollectors, ",") {
-		param = strings.TrimSpace(param)
-		parts := strings.SplitN(param, ".", 2)
-		if len(parts) != 2 {
-			return nil, fmt.Errorf("invalid collector name: %s", param)
-		}
-		if parts[0] != "stats" {
-			return nil, fmt.Errorf("invalid collector prefix: %s", parts[0])
-		}
-
-		name := parts[1]
-		c, has := statsRegistry[name]
-		if !has {
-			return nil, fmt.Errorf("unknown stats collector: %s", name)
-		}
-		ex.Use(c(*namespace))
-	}
-	return ex, nil
-}
-
-func normalizeURL(ustr string) (string, error) {
-	ustr = strings.ToLower(ustr)
-	if !strings.HasPrefix(ustr, "https://") && !strings.HasPrefix(ustr, "http://") {
-		ustr = "http://" + ustr
-	}
-
-	u, err := url.Parse(ustr)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println(u, u.String())
-	if u.Path == "" {
-		u.Path = "v1/stats"
-	}
-	return u.String(), nil
 }
